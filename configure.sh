9ff9b2907b0a762449ac275943da35d46bd8043b
Sayan Shankhari <sayan0shankhari@gmail.com>
2025-09-07 20:19:05 +0530

    fix (install): fix install script
    Able to install in the parent directory.
    
    Change the installation relative location to the project's
    parent directory. Fix the configuration script accordingly.
    
    Resolves: #8

--- configure.sh
+++ configure.sh
@@ -1,5 +1,4 @@
 wget -O- "https://raw.githubusercontent.com/TheScienceUniverse/DSA/refs/heads/master/install.sh" | sh
-cd ./codec/
 
 mkdir -p ./obj/ ./bin/ ./lib/ ./log/
 
--- install.sh
+++ install.sh
@@ -1,29 +1,39 @@
-echo "Cleaning residue..."
-rm -rf ./codec/
+prj_dir=$(pwd)
+cd ../
 
-echo "Fetching latest version..."
-git clone https://github.com/TheScienceUniverse/codec.git
+if [ -d "./codec/" ]
+then
+	cd ./codec/
+
+	if [ ! -d "./.git/" ]
+	then
+		git init
+		git remote add origin https://github.com/TheScienceUniverse/codec.git
+	fi;
+
+	echo "Updating codec project..."
+	git pull origin master
+
+	cd ../
+else
+	echo "Fetching codec project..."
+	git clone https://github.com/TheScienceUniverse/codec.git --single-branch --depth 1
+fi;
 
 cd ./codec/
 
-sh ./configure.sh
-
-echo "Setting up directories..."
-mkdir -p ./obj/ ./lib/ ./bin/
+echo "Configuring project for fresh build..."
+sh configure.sh
 
 echo "Building library..."
 make clean
-make all
+make again
+make check &> /dev/null
 
-echo "Cleaning up...";
-mv ./DSA/inc/ ./tmp/
-rm -rf ./DSA/*
-mv ./tmp/ ./DSA/inc/
-rm -rf ./archive/
-rm -rf ./.git/ ./.github/ ./aft/ ./bin/ ./cov/ ./doc/ ./font/ ./log/ ./media/ ./obj/ ./src/ ./tst/
-rm -f .gitignore LICENSE
-rm -f Makefile rerun
-rm -f *.c *.sh
+echo "Creating log directory (DON'T DELETE)..."
+mkdir -p ../log/
+#cp ./inc/* ../inc/
+#cp ./lib/libdsa.so ../lib/
 
-cd ../
+cd $prj_dir
 echo "...Installation Done!"
